ff3ff505b01cabccd04debbac06c41c8c310cfc0
Ritesh_Kumar <ritesh2803@outlook.com>
2021-05-28 18:05:22 +0530

    first update 28May

---
+++ src/com/rit/oracle/jdbc/SelectTest.java
@@ -0,0 +1,31 @@
+package com.rit.oracle.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class SelectTest {
+
+	public static void main(String[] args)throws Exception {
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+		Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","root");
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery("select * from student");
+		
+		while(resultSet.next() != false)
+		{
+			//System.out.println(" "+resultSet.getInt(1)+" "+resultSet.getString(2)+" "+resultSet.getString(3)+" "+resultSet.getFloat(4)+" ");
+			//System.out.println(" "+resultSet.getString(1)+" "+resultSet.getString(2)+" "+resultSet.getString(3)+" "+resultSet.getString(4)+" ");
+			//System.out.println(" "+resultSet.getInt("sno")+" "+resultSet.getString("sname")+" "+resultSet.getString("sadd")+" "+resultSet.getFloat("avg")+" ");
+			System.out.println(" "+resultSet.getString("sno")+" "+resultSet.getString("sname")+" "+resultSet.getString("sadd")+" "+resultSet.getString("avg")+" ");
+
+		}
+		resultSet.close();
+		statement.close();
+		connection.close();
+	}
+
+}
